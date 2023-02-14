0499ce02d18eb07587638c2c0b945794dc07aa7a
vegetax <56632283@qq.com>
2023-02-14 22:21:09 +0800

    1.0

--- Find5QuestionAndAnswer.py
+++ Find5QuestionAndAnswer.py
@@ -19,14 +19,27 @@ def gpt_3(prompt):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
-        max_tokens=7,
+        max_tokens=1000,
         top_p=1,
         n=1,
         temperature=0
     )
     text = response['choices'][0]['text'].strip()
-    print(text)
+    # print(text)
     return text
 
 
-questions = gpt_3("how are you")
+info = open_file("input.txt")
+prompt1 = open_file("prompt1.txt").replace('<<FEED>>', info)
+print(prompt1)
+questions = gpt_3(prompt1)
+print(questions)
+save_file("questions.txt", questions)
+
+questions2 = open_file("questions.txt")
+prompt2 = open_file("prompt2.txt").replace(
+    '<<FEED>>', info).replace('<<QS>>', questions2)
+print(prompt2)
+answers = gpt_3(prompt2)
+print(answers)
+save_file('answers.txt', answers)
